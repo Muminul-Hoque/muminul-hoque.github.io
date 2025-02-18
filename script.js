ec310da5926a35b71e1acc9b8d1c333880366b29
Muminul Hoque <125432156+Muminul-Hoque@users.noreply.github.com>
2025-02-18 16:34:08 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const images = document.querySelectorAll(".certificate-img");
+  const images = document.querySelectorAll(".achievement-img");
 
   setTimeout(() => {
     images.forEach(img => img.classList.add("show"));
-  }, 500);  // Delay for smooth animation
+  }, 500);  // Adds delay for smooth animation
 });
+
